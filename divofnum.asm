6227e923498c66fc0adb54aee42412550dc21aac
adityaaggarwal19 <33057627+adityaaggarwal19@users.noreply.github.com>
2018-11-02 00:55:56 +0530

    Add files via upload

---
+++ divofnum.asm
@@ -0,0 +1,90 @@
+;program to take input of numbers and perform division operation (8 digit)(32 bit)
+.MODEL SMALL
+.386
+.CODE
+.STARTUP
+;   TAKING INPUT OF 32 BIT NUMBERS
+MOV ECX,8
+MOV EDX,0
+INP1:
+    SHL EDX,4
+    MOV AH,01
+    INT 21H
+    CMP AL,39H
+    JBE L
+       SUB AL,7H
+    L: SUB AL,30H
+    ADD DL,AL
+    LOOP INP1  
+    
+MOV ESP,EDX;   MOVING EDX TO ANY REGISTER
+MOV DL,'/'
+MOV AH,02
+INT 21H
+
+MOV ECX,8
+MOV EBX,0
+INP2:
+    SHL EBX,4
+    MOV AH,01
+    INT 21H
+    CMP AL,39H
+    JBE L1
+        SUB AL,7H
+    L1: SUB AL,30H
+    ADD BL,AL
+    LOOP INP2
+   
+MOV EAX,ESP
+MOV EDX,0
+DIV EBX ;   DIVIDING THE EAX
+
+MOV EBP,EDX
+MOV ESP,EAX
+; seperating with R: operator 
+MOV DL,'R'
+MOV AH,02
+INT 21H 
+MOV DL,':'
+MOV AH,02
+INT 21H
+
+;   DISPLAYING EDX:EAX RESULT
+MOV ECX,8
+MOV EDX,0
+MOV EAX,0
+DIS1:
+    MOV EDX,EBP
+    AND EDX,0F0000000H
+    ROL EDX,4
+    CMP DL,9
+    JBE L2
+        ADD DL,7H
+    L2: ADD DL,30H
+    MOV AH,02
+    INT 21H
+    SHL EBP,4
+    LOOP DIS1
+; seperating with Q: operator 
+MOV DL,'Q'
+MOV AH,02
+INT 21H 
+MOV DL,':'
+MOV AH,02
+INT 21H
+MOV ECX,8
+MOV EDX,0
+DIS2:
+    MOV EDX,ESP
+    AND EDX,0F0000000H
+    ROL EDX,4
+    CMP DL,9
+    JBE L3
+        ADD DL,7H
+        L3: ADD DL,30H
+    MOV AH,02
+    INT 21H
+    SHL ESP,4
+    LOOP DIS2
+.EXIT
+END
