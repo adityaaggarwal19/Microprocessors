cf7c11fd9221b287de0ed08c5f6f289a45847b1f
adityaaggarwal19 <33057627+adityaaggarwal19@users.noreply.github.com>
2018-11-03 15:25:22 +0530

    Add files via upload

---
+++ subofnum.asm
@@ -0,0 +1,56 @@
+;program to take input of numbers and perform substraction operation (8 digit)(32 bit)
+.MODEL SMALL
+.386
+.CODE
+.STARTUP
+; taking input of four digit in BX
+MOV ECX,8
+MOV EBX,0
+INP1:
+    SHL EBX,4
+    MOV AH,01
+    INT 21H
+    CMP AL,39H
+    JBE L1
+        SUB AL,7H
+    L1: SUB AL,30H
+    ADD BL,AL
+    LOOP INP1
+    ; seperating with - operator  
+    MOV DL,'-'
+MOV AH,02
+INT 21H
+    
+; taking input of four digit in DX
+MOV ECX,8
+MOV EDX,0
+INP2:
+    SHL EDX,4
+    MOV AH,01
+    INT 21H
+    SUB AL,30H
+    ADD DL,AL
+    LOOP INP2
+;ADD TWO NUMBERS
+SUB EBX,EDX
+; seperating with = operator  
+MOV DL,'='
+MOV AH,02
+INT 21H
+
+MOV ECX,8
+MOV EDX,0
+DIS:
+    MOV EDX,EBX
+    AND EDX,0F0000000H
+    ROL EDX,4
+    CMP DL,9
+    JBE L2
+        ADD DL,7H
+    L2: ADD DL,30H
+    MOV AH,02
+    INT 21H
+    SHL EBX,4
+    LOOP DIS
+.EXIT
+END
